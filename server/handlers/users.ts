76ea57644159415cf36c746ada12828fcd93c497
rowjay007 <rowlives@gmail.com>
2023-02-06 08:55:33 +0100

    fix: update users typescript module

--- server/handlers/users.ts
+++ server/handlers/users.ts
@@ -1,7 +1,19 @@
 import prisma from "../db";
 import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
-export const createNewUser = async (req, res) => {
+interface Request {
+  body: {
+    username: string;
+    password: string;
+  };
+}
+
+interface Response {
+  json: (data: { token?: string; message?: string }) => void;
+  status: (statusCode: number) => Response;
+}
+
+export const createNewUser = async (req: Request, res: Response) => {
   const user = await prisma.user.create({
     data: {
       username: req.body.username,
@@ -13,12 +25,18 @@ export const createNewUser = async (req, res) => {
   res.json({ token });
 };
 
-export const signin = async (req, res) => {
+export const signin = async (req: Request, res: Response) => {
   const user = await prisma.user.findUnique({
     where: {
       username: req.body.username,
     },
   });
+
+  if (!user) {
+    res.status(401);
+    res.json({ message: "nope" });
+    return;
+  }
 
   const isValid = await comparePasswords(req.body.password, user.password);
 
